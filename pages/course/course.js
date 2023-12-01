1d14cc53d16e3fe382293b852acd42db7558dba3
kocpiepait <f.barahama01@gmail.com>
2023-12-01 15:32:38 +0700

    main page

---
+++ pages/course/course.js
@@ -0,0 +1,10 @@
+// Toggle class active
+
+const navbar = document.querySelector
+
+('.navbar');
+// ketika menu diclick
+
+document.querySelector('#menu' ).onclick = () => {
+    navbar.classList.toggle('active');
+};
